543e82e43c93d67aca4f56119422c2e0ab2d0cf0
hwixley <hwixley1@gmail.com>
2021-03-02 13:38:16 +0000

    Starting on task 1

--- STCOURSEWORK2021/src/test/java/st/Task1_Functional.java
+++ STCOURSEWORK2021/src/test/java/st/Task1_Functional.java
@@ -3,8 +3,26 @@ package st;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Before;
 
 public class Task1_Functional {
-	
 
+private Parser parser;	
+
+	@Before
+	public void set_up() {
+		parser = new Parser();
+	}
+	
+	@Test
+	public void test_filename_arg() {
+		parser.add("filename", Parser.STRING);
+		parser.parse("--filename 1.txt");
+		assertEquals(parser.getString("filename"), "1.txt");
+	}
+	
+	@Test
+	public void test_output_arg() {
+		parser.add("output", "o", Parser.STRING);
+	}
 }
