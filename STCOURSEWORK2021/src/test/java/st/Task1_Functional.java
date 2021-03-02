407f601031ed341e848b73d0c58a5adb83882074
hwixley <hwixley1@gmail.com>
2021-03-02 13:54:31 +0000

    Starting on task 1

--- STCOURSEWORK2021/src/test/java/st/Task1_Functional.java
+++ STCOURSEWORK2021/src/test/java/st/Task1_Functional.java
@@ -24,5 +24,41 @@ private Parser parser;
 	@Test
 	public void test_output_arg() {
 		parser.add("output", "o", Parser.STRING);
+		parser.parse("--output out.txt");
+		assertEquals(parser.getString("output"), "out.txt");
+	}
+	
+	@Test
+	public void test_input_args() {
+		parser.add("filename", Parser.STRING);
+		parser.add("output", "o", Parser.STRING);
+		parser.parse("--filename 2.txt --output output.txt");
+		// Full option names
+		assertEquals(parser.getString("filename"), "2.txt");
+		assertEquals(parser.getString("output"), "output.txt");
+		// Output option shortcut
+		assertEquals(parser.getString("o"), "output.txt");
+	}
+	
+	@Test
+	public void test_exclude_tags() {
+		
+	}
+	
+	@Test
+	public void test_boolean_type_true() {
+		parser.add("option", Parser.BOOLEAN);
+		
+		parser.parse("--option 9rfg");
+		assertEquals(parser.getString("option"), true);
+		
+		parser.parse("--option 00");
+		assertEquals(parser.getString("option"), true);
+		
+		parser.parse("--option False");
+		assertEquals(parser.getString("option"), true);
+		
+		parser.parse("--option");
+		assertEquals(parser.getString("option"), true);
 	}
 }
