fc30889dbff57c230b7957404e514fe9d106ae5a
hwixley <hwixley1@gmail.com>
2021-03-05 10:25:42 +0000

    Caught bug11 part 1

--- ST-COURSEWORK1/src/test/java/st/Task1_Functional.java
+++ ST-COURSEWORK1/src/test/java/st/Task1_Functional.java
@@ -15,6 +15,17 @@ private Parser parser;
 	}
 	
 	@Test
+	public void bug1() { // 2 points
+		parser.add("", Parser.STRING);
+		assertEquals(parser.parse("--"), 0); //BUG: cannot parse empty name (spec does not say min length)
+	}
+	
+	@Test (expected = RuntimeException.class)
+	public void bug2() { // 1 point
+		parser.add("1test", "t", Parser.STRING);
+	}
+	
+	@Test
 	public void bug3() { // 1 point
 		parser.add("output","o", Parser.STRING);
 		parser.add("filename", Parser.STRING);
@@ -24,25 +35,86 @@ private Parser parser;
 		assertEquals(parser.getString("output"), "2.txt");
 	}
 	
-	@Test (expected = RuntimeException.class)
-	public void bug2() { // 1 point
-		parser.add("1test", "t", Parser.STRING);
+	@Test
+	public void bug4() {
+		
 	}
 	
 	@Test
-	public void bug1() { // 2 points
-		parser.add("", Parser.STRING);
-		assertEquals(parser.parse("--"), 0); //BUG: cannot parse empty name (spec does not say min length)
+	public void bug5() { // 1 point
+		parser.add("file","f", Parser.CHAR);
+		//parser.add("fix","f", Parser.STRING);
+		assertEquals(parser.parse("--file= "), 0);
+		assertEquals(parser.getChar("file"), " ");
 	}
 	
-	
+	@Test
+	public void bug6() {
+		
+	}
 	
 	@Test
+	public void bug7() {
+		
+	}
+	
+	@Test
+	public void bug8() {
+		
+	}
+	
+	@Test
+	public void bug9() {
+		
+	}
+	
+	@Test
+	public void bug10() { // 3 points
+		parser.add("file","f", Parser.CHAR);
+		parser.add("f", Parser.BOOLEAN);
+		//parser.add("fix","f", Parser.STRING);
+		assertEquals(parser.parse("--file=="), 0);
+		assertEquals(parser.getString("f"), "");
+	}
+	
+	@Test
+	public void bug11() { // 3 points
+		parser.add("output", "oUt", Parser.STRING);
+	  	parser.add("oUt", "out", Parser.STRING);
+	  	parser.parse("-oUt=1.txt -out=12"); // i assigned a value using shortcut
+	  	assertEquals(parser.getString("output"),"1.txt"); // I got the value with shortcut
+	  	assertEquals(parser.getString("oUt"), "12");
+	}
+	
+	@Test
+	public void bug12() {
+		
+	}
+	
+	@Test
+	public void bug13() { // 2 points
+		parser.add("file","f", Parser.CHAR);
+		parser.add("f", Parser.BOOLEAN);
+		//parser.add("fix","f", Parser.STRING);
+		assertEquals(parser.parse("-f=value"), 0);
+		assertEquals(parser.getString("f"), "");
+	}
+	
+	@Test
+	 public void shortcut() {
+	  parser.add("output", "oUt", Parser.STRING);
+	  parser.add("oUt", "out", Parser.STRING);
+	  parser.parse("-oUt=1.txt -out=12 --output=1nine"); // i assigned a value using shortcut
+	  assertEquals(parser.getString("output"),"1nine"); // I got the value with shortcut
+	  assertEquals(parser.getString("oUt"), "12");
+	 }
+	
+	/*@Test
 	public void bug() {
 		parser.add("F", Parser.STRING);
 		parser.add("f", Parser.STRING);
 		parser.parse("--F value --f");
 		assertEquals(parser.getString("F"), "value");
 		assertEquals(parser.getString("f"), "");
-	}
+	}*/
 }
